55174d06737822e0d5e69332587911b5aa5e429f
Senthil Kumar Thangavelu <kingjuliyen@gmail.com>
2018-06-11 23:13:30 +0530

    002-Quick-hands-on exercises first program

---
+++ assignments/002-Quick-hands-on/002-print-message.answer.c
@@ -0,0 +1,19 @@
+
+
+    // to compile use
+      // gcc 002-print-message.template.c
+
+    // to run the program use
+      // ./a.out
+
+    #include <stdio.h>
+
+    int main()
+    {
+      printf("My first program in C");
+      printf("\n");
+    }
+
+
+
+    ////
---
+++ assignments/002-Quick-hands-on/002-print-message.template.c
@@ -0,0 +1,19 @@
+
+
+    // to compile use
+      // gcc 002-print-message.template.c
+
+    // to run the program use
+      // ./a.out
+
+    #include <stdio.h>
+
+    int main()
+    {
+      printf(); // Add string My "first program in C" inside printf
+      printf("\n");
+    }
+
+
+
+    ////
