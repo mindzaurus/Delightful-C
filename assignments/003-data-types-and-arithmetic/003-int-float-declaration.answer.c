2a0c5913f468fe148129984404abf9aa7d2421ef
Senthil Kumar Thangavelu <kingjuliyen@gmail.com>
2018-06-13 14:43:25 +0530

    exercise 1 for data types

---
+++ assignments/003-data-types-and-arithmetic/003-int-float-declaration.answer.c
@@ -0,0 +1,17 @@
+
+
+
+  // Solution to 003-int-float-declaration.template.c
+  
+  #include <stdio.h>
+
+  int main()
+  {
+    //declare days_in_january below and set value to 31
+    int days_in_january = 31;
+    //declare value_of_pi below and set value to 3.14
+    float value_of_pi = 3.14f;
+
+    printf("value of integer is = %d \n", days_in_january);
+    printf("value of float is = %.2f \n", value_of_pi);
+  }
---
+++ assignments/003-data-types-and-arithmetic/003-int-float-declaration.template.c
@@ -0,0 +1,21 @@
+
+  // This program won't compile
+  // Follow 2 steps to complete this exercise
+  // use this template and
+
+  // 1) declare a integer with name days_in_january and set the value to 31
+  // 2) declare a float with name value_of_pi and set it to 3.14
+  // don't modify printfs they are fine if you change variable names then modify
+  // hint: remember data types int, float and the assignment operator =
+
+  #include <stdio.h>
+
+  int main()
+  {
+    //declare days_in_january below and set value to 31
+
+    //declare value_of_pi below and set value to 3.14
+    
+    printf("value of integer is %d \n", days_in_january);
+    printf("value of float is %.2f \n", value_of_pi);
+  }
