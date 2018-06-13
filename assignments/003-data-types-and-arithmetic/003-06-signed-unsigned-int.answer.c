37232920dc10ec5b0dffa725f993ec730905e915
Senthil Kumar Thangavelu <kingjuliyen@gmail.com>
2018-06-13 19:44:27 +0530

    Adding exercise for signed, unsigned int values

---
+++ assignments/003-data-types-and-arithmetic/003-06-signed-unsigned-int.answer.c
@@ -0,0 +1,22 @@
+
+/*
+  This program will
+
+  1) print a signed integer variable named
+     signed_value after setting it to -45
+  2) print a unsigned integer variable named
+     unsigned_value after setting it to 743
+*/
+
+#include <stdio.h>
+
+int main()
+{
+  // 1) declare and set variable signed_value to -45 and print it
+  int signed_value = -45;
+  // 2) declare and set variable unsigned_value to 743 and print it
+  unsigned int unsigned_value = 743;
+
+  printf("signed_value is set to = %d \n", signed_value);
+  printf("unsigned_value is set to = %u \n", unsigned_value);
+}
