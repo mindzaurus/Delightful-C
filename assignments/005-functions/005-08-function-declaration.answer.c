876d64f866e52513ca125358b3b4c586b4f299ac
Senthil Kumar Thangavelu <kingjuliyen@gmail.com>
2018-06-15 23:21:11 +0530

    Added function declaration exercise

---
+++ assignments/005-functions/005-08-function-declaration.answer.c
@@ -0,0 +1,34 @@
+/*
+  UNDERSTANDING FUNCTION DECLARATIONS - COMPILER LIKES HINTS AND CLUES
+
+  1) Write main function which calls a function function_at_the_end
+  2) Write a function function_at_the_end that takes void as argument
+     and returns void, this function should be written after main function
+  3) Compile the program, you should see a warning or error
+  4) Add a function declaration for function_at_the_end before the function main
+
+*/
+
+// Don't get confused by seeing step 4) first you will have to get back to step 4)
+// after finishing steps 1) , 2) and 3)
+// 4) Add a function declaration for function_at_the_end before the function main
+// FUNCTION DECLARATION TO BE DONE HERE AS LAST STEP
+void function_at_the_end(void);
+
+// 1) Write main function which calls a function function_at_the_end
+int main()
+{
+  function_at_the_end();
+}
+
+// 2) Write a function function_at_the_end that takes void as argument
+//    and returns void, this function should be written after main function
+void function_at_the_end(void)
+{
+
+}
+
+// 3) Compile the program, you should see a warning or error
+//    gcc 7.0 -> warning
+//    clang -> error
+//    visual studio -> error
