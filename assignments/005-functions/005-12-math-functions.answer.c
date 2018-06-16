2263e9a89192cf910193296a3508650b64f93210
Senthil Kumar Thangavelu <kingjuliyen@gmail.com>
2018-06-16 11:30:00 +0530

    Added math function exercise

---
+++ assignments/005-functions/005-12-math-functions.answer.c
@@ -0,0 +1,44 @@
+
+/*
+  UNDERSTANDING C INBUILT FUNCTIONS - MATH LIBRARY: POWER FUNCTION EXAMPLE
+  1) Include math.h header file
+  2) Write a function x_to_the_power_y which takes two double arguments x and y
+     and returns a double return value
+  3) Implement x_to_the_power_y where x should be raised the power of y
+     e.g x_to_the_power_y(3.0, 4.0) should produce result 81.0
+     hint: use the pow function from math.h
+  4) call the function x_to_the_power_y from main raise 2 to the power of 7 and
+     store the result in double variable two_power_seven
+  5) Print the value of two_power_seven
+  6) Compile the program in gcc using command gcc 005-12-math-functions.c use -lmath flag if required
+  7) Running the program should give two_power_seven is = 128.000000
+*/
+
+// 1) Include math.h header file
+#include <math.h>
+#include <stdio.h>
+
+// 2) Write a function x_to_the_power_y which takes two double arguments x and y
+//    and returns a double return value
+float x_to_the_power_y(float x, float y)
+{
+  // 3) Implement x_to_the_power_y where x should be raised the power of y
+  //    e.g x_to_the_power_y(3.0, 4.0) should produce result 81.0
+  //    hint: use the pow function from math.h
+  float result = pow(x, y);
+  return result;
+}
+
+int main()
+{
+  // 4) call the function x_to_the_power_y from main raise 2 to the power of 7 and
+  //    store the result in double variable two_power_seven
+  float two_power_seven = pow(2.0, 7.0);
+
+  // 5) Print the value of two_power_seven, note %lf for printing double
+  //    pass correct parameters to printf
+  printf("two_power_seven is = %lf\n", two_power_seven);
+}
+
+// 6) Compile the program in gcc using command gcc 005-12-math-functions.c use -lmath flag if required
+// 7) Running the program should give two_power_seven is = 128.000000
