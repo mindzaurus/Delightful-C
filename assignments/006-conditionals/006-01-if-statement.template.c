bac51c61e2df432c127b2e707a31ccc6a523a709
Senthil Kumar Thangavelu <kingjuliyen@gmail.com>
2018-06-16 18:55:14 +0530

    Added if statement exercise

---
+++ assignments/006-conditionals/006-01-if-statement.template.c
@@ -0,0 +1,64 @@
+/*
+  UNDERSTANDING IF STATEMENT IN - QUICK MULTIPLY FUNCTION
+
+  1) Write a function if_multiplier which takes in two int arguments x and y
+     it should return an int value
+  2) Function if_multiplier should check whether x is 1, if x is 1 then return y
+     This is because y * 1 is y
+  3) if x is not 1 then return x * y
+  4) Call the if_multiplier with x as 3 and y as 2 from inside main function
+     Store the the returned value in int variable result
+  5) Print the variable result this should be 6
+  6) Call the if_multiplier with x as 1 and y as 2 from inside main function
+     Store the the returned value in int variable result
+  7) Print the variable result this should be 2
+
+  The important idea to understand if x is equal to 1
+  then we don't do any multiplication as the if check is performed
+
+  STEP 2) IS THE IMPORTANT PART OF THIS PROGRAM
+*/
+
+/*
+  Output shall look like This
+  3 * 2 result = 6
+  if_multiplier: x == 1 so returning y directly no multiplication
+  1 * 2 result = 2
+*/
+
+#include <stdio.h>
+
+// 1) Write a function if_multiplier which takes in two int arguments x and y
+//    it should return an int value
+
+{ // Begin Function if_multiplier
+
+  // 2) Function if_multiplier should check whether x is equal to 1,
+  //    if x is equal to 1 then return y, hint: use == operator to check whether x is 1
+  //    This is because y * 1 is y
+
+
+  // 3) if x is not 1 then return x * y
+
+} // End Function if_multiplier
+
+
+int main()
+{
+  // 4) Call the if_multiplier with x as 3 and y as 2 from inside main function
+  //    Store the the returned value in int variable result
+
+
+  // 5) Print the variable result this should be 6
+
+
+  // 6) Call the if_multiplier with x as 1 and y as 2 from inside main function
+  //    Store the the returned value in int variable result
+
+
+  // 7) Print the variable result this should be 2
+
+}
+
+
+//
