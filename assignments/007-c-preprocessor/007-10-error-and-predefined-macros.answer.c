f854cb8c986226ef1338245f49649de5c51d8d6f
Senthil Kumar Thangavelu <kingjuliyen@gmail.com>
2018-06-18 12:54:33 +0530

    Added error and predefined macro exercises

---
+++ assignments/007-c-preprocessor/007-10-error-and-predefined-macros.answer.c
@@ -0,0 +1,44 @@
+
+/*
+  UNDERSTANDING ERROR AND PREDEFINED MACRO DIRECTIVES - EXPERIMENTAL FEATURE AND FILE, LINE NUMBERS EXAMPLES
+  1) Add a error directive stating "EXPERIMENTAL UNSTABLE CODE USED" within #ifdef flag EXPERIMENTAL_CODE
+  2) Add main function and compile the program using command
+     gcc 007-09-error-and-predefined-macros.template.c
+     Compilation should be fine now without any error
+  3) Compile again with following command
+     gcc -DEXPERIMENTAL_CODE 007-09-error-and-predefined-macros.template.c
+     You should get an error like error: #error "EXPERIMENTAL UNSTABLE CODE USED"
+  4) Inside main try to print the filename, function and line number using the
+     __FILE__, __FUNCTION__ and __LINE__ inbuilt macros
+  5) Compile using following command
+     gcc 007-09-error-and-predefined-macros.template.c
+  6) Execute the program You should see output like following line number may vary though
+     In file: 007-09-error-and-predefined-macros.template.c , function: main , line: 29
+*/
+
+#include <stdio.h>
+
+// 1) Add a error directive stating "EXPERIMENTAL UNSTABLE CODE USED" within #ifdef flag EXPERIMENTAL_CODE
+#ifdef EXPERIMENTAL_CODE
+  #error "EXPERIMENTAL UNSTABLE CODE USED"
+#endif
+
+// 2) Add main function and compile the program using command
+//    gcc 007-09-error-and-predefined-macros.template.c
+//    Compilation should be fine now without any error
+int main()
+{
+  // 3) Compile again with following command
+  //    gcc -DEXPERIMENTAL_CODE 007-09-error-and-predefined-macros.template.c
+  //    You should get an error like error: #error "EXPERIMENTAL UNSTABLE CODE USED"
+
+  // 4) Inside main try to print the filename, function and line number using the
+  //    __FILE__, __FUNCTION__ and __LINE__ inbuilt macros
+  printf("In file: %s , function: %s , line: %d \n", __FILE__, __FUNCTION__, __LINE__);
+
+  // 5) Compile using following command
+  //    gcc 007-09-error-and-predefined-macros.template.c
+
+  // 6) Execute the program You should see output like following line number may vary though
+  //    In file: 007-09-error-and-predefined-macros.template.c , function: main , line: 29
+}
