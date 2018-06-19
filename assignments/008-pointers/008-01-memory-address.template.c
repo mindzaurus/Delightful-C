b8b4f333766850e5cd52382bdb091cf877acecc5
Senthil Kumar Thangavelu <kingjuliyen@gmail.com>
2018-06-19 13:50:41 +0530

    Added memory address exercise for & address operator

---
+++ assignments/008-pointers/008-01-memory-address.template.c
@@ -0,0 +1,43 @@
+/*
+  UNDERSTANDING MEMORY LOCATIONS AND ADDRESSES - WHO LIVES WHERE USING & ADDRESS OPERATOR
+
+  1) Declare a global variable global_value of type int and assign a value of 5
+  2) Inside the main function declare a local variable local_int_val of type int and assign a value of -1
+  3) Inside the main function print the address of global variable global_value, use %p in printf
+     hint: to get the address of variable global_value's memory location use '&' address operator
+  4) Inside the main function print the address of local variable local_int_val, use %p in printf
+     hint: to get the address of variable local_int_val's memory location use '&' address operator
+*/
+
+
+/*
+  Output should look something like this, though exact address on your system might vary
+  Address of global_value 0x100f6f018
+  Address of local_int_val 0x7ffeeec9172c
+*/
+
+#include <stdio.h>
+
+// 1) Declare a global variable global_value of type int and assign a value of 5
+
+
+int main()
+{
+  // 2) Inside the main function declare a local variable local_int_val
+  //    of type int and assign a value of -1
+
+
+  // 3) Inside the main function print the address
+  //    of global variable global_value, use %p in printf
+  //    hint: to get the address of variable global_value's
+  //    memory location use '&' address operator
+
+
+  // 4) Inside the main function print the address
+  //    of local variable local_int_val, use %p in printf
+  //    hint: to get the address of variable local_int_val's
+  //    memory location use '&' address operator
+
+}
+
+//
