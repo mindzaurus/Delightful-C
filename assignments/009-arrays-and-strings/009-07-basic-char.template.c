a459ac050d753391258ab1e5ac77b16bc09a43ca
Senthil Kumar Thangavelu <kingjuliyen@gmail.com>
2018-06-22 23:39:27 +0530

    Added basic char type exercise

---
+++ assignments/009-arrays-and-strings/009-07-basic-char.template.c
@@ -0,0 +1,40 @@
+/*
+  UNDERSTANDING CHAR DATA TYPE USED IN STRINGS - INSPECTING THE DECIMAL VALUES OF CHARACTERS
+
+  1) Declare a char c1 and initialize it to 'A', use single quoted A
+  2) Declare a char c2 and initialize it to 'Z', use single quoted Z
+  3) Declare a char c3 and initialize it to '\0',
+     use single quoted back slash \ followed by a 0 (zero character)
+  4) Print c1 using format specifiers %c, %d, %x
+  5) Print c2 using format specifiers %c, %d, %x
+  6) Print c3 using format specifiers %c, %d, %x
+
+  You can also see manual page by typing 'man ascii' and print your favourite characters
+*/
+
+#include <stdio.h>
+
+int main()
+{
+
+  // 1) Declare a char c1 and initialize it to 'A', use single quoted A
+
+
+  // 2) Declare a char c2 and initialize it to 'Z', use single quoted Z
+
+
+  // 3) Declare a char c3 and initialize it to '\0',
+  //    use single quoted back slash \ followed by a 0 (zero character)
+
+
+  // 4) Print c1 using format specifiers %c, %d, %x
+
+
+  // 5) Print c2 using format specifiers %c, %d, %x
+
+
+  // 6) Print c3 using format specifiers %c, %d, %x
+  // null terminator won't be printed
+
+
+}
