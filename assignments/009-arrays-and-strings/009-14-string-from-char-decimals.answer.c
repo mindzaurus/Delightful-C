6899a789625d9f02ee0f54c797f88eab7f4bd8b6
Senthil Kumar Thangavelu <kingjuliyen@gmail.com>
2018-06-23 11:32:03 +0530

    Added string construction using decimal values exercise

---
+++ assignments/009-arrays-and-strings/009-14-string-from-char-decimals.answer.c
@@ -0,0 +1,21 @@
+/*
+  UNDERSTANDING C STRINGS AND ASCII SET RELATIONSHIP -
+    DEFINING A STRING USING ASCII CHAR DECIMAL VALUES
+  1) Declare and initialize a C string as array of chars using decimal values
+     of characters 'A', 'B' and 'C'. Remember to null terminate using char value of 0
+     The name of array shall be str_arr, Use curly braces initializer { }
+  2) Print str_arr using %s in printf and the output shoul be printed as "ABC"
+*/
+
+#include <stdio.h>
+
+int main()
+{
+  // 1) Declare and initialize a C string as array of chars using decimal values
+  //    of characters 'A', 'B' and 'C'. Remember to null terminate using char value of 0
+  //    The name of array shall be str_arr, Use curly braces initializer { }
+  char str_arr [ ] = { 65, 66, 67, 0 };
+
+  // 2) Print str_arr using %s in printf and the output shoul be printed as "ABC"
+  printf("str_arr == %s \n", str_arr);
+}
