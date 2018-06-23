6baad755d314a1f5d808a64bfa7c31c81e1d9a72
Senthil Kumar Thangavelu <kingjuliyen@gmail.com>
2018-06-23 14:07:03 +0530

    Added main with argc argv exercise

---
+++ assignments/009-arrays-and-strings/009-21-main-argc-argv.template.c
@@ -0,0 +1,33 @@
+/*
+  UNDERSTANDING THE MAIN, ARGC AND ARGV - PASSING ARGUMENTS TO PROGRAM
+  1) Write a main function which takes two arguments int argc and an array of
+     strings (char *)
+  2) Print the number of arguments passed to the program, int argc to be printed
+  3) Print the first, second and third arguments passed to the program
+  4) Run the program using command
+     ./a.out --name=Tom --type=Cat
+  5) Copy the a.out file as cartoon_maker and run the command below,
+     observe how the first argument changes now compared to step 3)
+     ./cartoon_maker --name=Tom --type=Cat
+*/
+
+#include <stdio.h>
+
+// 1) Write a main function which takes two arguments int argc and an array of
+//    strings (char *)
+int main(int argc, char * argv[])
+{
+  // 2) Print the number of arguments passed to the program, int argc to be printed
+
+
+  // 3) Print the first, second and third arguments passed to the program
+
+
+  // 4) Run the program using command
+  //    ./a.out --name=Tom --type=Cat
+
+  // 5) Copy the a.out file as cartoon_maker and run the command below,
+  //    observe how the first argument changes now compared to step 3)
+  //    ./cartoon_maker --name=Tom --type=Cat
+
+}
